fdf6a419d83b769226e699ddb5b2d235d5aa53ec
Joseph Baca <josephcbaca@gmail.com>
2020-06-28 22:57:41 -0700

    Adding inquirer question layout.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 let mysql = require("mysql");
 let express = require("express");
 require('dotenv').config({ path: "/Users/josep/dev/AdditionalFiles/.env" });
+const inquirer = require("inquirer");
 
 let PORT = process.env.PORT || 8080;
 
@@ -16,17 +17,60 @@ let connection = mysql.createConnection({
 
   // Your password
   password: process.env.DB_PASS,
-  database: "playlist_db"
+  database: "character"
 });
 
-connection.connect(function(err) {
+const questions = [
+  {
+    type: "list",
+    name: "questions",
+    message: "Choose the badge you wish to attach to your Read Me.",
+    choices: [
+      "View All Employees",
+      "View All Employees By Department",
+      "View All Employees By Manager",
+      // "Add Employee",
+      // // What is the employee's firstname?
+      // // What is the employee's last name?
+      // // What is the employee's role?(Choose from already entered role)
+      // // Who is the employee's manager?
+      // "Remove Employee",
+      // // (Choose employee)
+      // "Update Employee Role",
+      // "Update Employee Manager"
+    ]
+  },
+];
+
+function promptUser(incoming) {
+  return inquirer.prompt(incoming);
+};
+
+async function init() {
+  try {
+
+    console.log("What would you like to do?");
+    
+    const data = promptUser(questions);
+console.log(data)
+    module.exports = data
+
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+init();
+
+// Connection to MySQL database
+connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
   connection.end();
 });
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
